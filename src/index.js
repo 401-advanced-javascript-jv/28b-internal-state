e019a156ddb4453f50d0da85d0c8114cbd4a4073
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-19 15:46:06 -0700

    display tracks input

--- src/index.js
+++ src/index.js
@@ -4,13 +4,34 @@ import ReactDOM from 'react-dom';
 import './styles.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.state.count = '';
+    this.state.name = '';
+  }
+
+  handleChange = (event) => {
+    const property = event.target.name;
+    const value = event.target.value;
+    this.setState({ [property]: value });
+  };
+
   render() {
     return (
       <React.Fragment>
         <div>
-          <div>Name: </div>
+          <div>Name: {this.state.name}</div>
           <div># of Updates: </div>
         </div>
+        <form>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.name}
+            name="name"
+          />
+        </form>
       </React.Fragment>
     );
   }
